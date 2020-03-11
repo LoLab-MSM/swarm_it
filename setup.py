df3ca40cd7f54a85f53e49c4adef3b1358d71334
Blake <blakeaw1102@gmail.com>
2020-03-11 14:31:02 -0500

    Updated the setup.py file.

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from distutils.core import setup
 
 setup(name='swarm_it',
-      version='0.1.1',
+      version='0.2.0',
       description='Utility to help setup PSO runs for PySB models.',
-      author='Blake A. Wilson',
-      author_email='blakeaw1102@gmail.com',
       url='https://github.com/LoLab-VU/swarm_it',
-      packages=['swarm_it'],
+      py_modules=['swarm_it'],
       license='MIT',
       keywords=['particle swarm optimization', 'model calibration', 'parameter estimation', 'pysb']
      )
